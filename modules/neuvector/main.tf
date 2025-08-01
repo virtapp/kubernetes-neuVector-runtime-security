578bcd361cfc631826950909355f9e4aaa338185
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-08-02 00:52:57 +0300

    update

--- modules/neuvector/main.tf
+++ modules/neuvector/main.tf
@@ -1,25 +1,32 @@
 
 
 resource "helm_release" "neuvector" {
-  name       = "neuvector-core"
-  namespace  = "neuvector"
+  name             = "neuvector-core"
+  namespace        = "neuvector"
   create_namespace = true
+
   repository = "https://neuvector.github.io/neuvector-helm/"
   chart      = "core"
-  version    = "2.6.6" # or latest version you want to deploy
+  version    = "2.6.6"
   # Optional: override default chart values
-  #values = [
-  #  file("./modules/neuvector/neuvector-values.yaml") # optional, for advanced config
-  #]
   values = [yamlencode({
     controller = {
       replicas = 1
     }
+
     enforcer = {
       containerd = {
         enabled = true
       }
+      dockerSock = {
+        enabled = false
+      }
     }
-})]
+
+    scanner = {
+      enabled = true
+    }
+  })]
 }
+
 
