5d3f0e84ca4f7787f08b6cec1d2530bc104b2033
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-08-01 23:52:23 +0300

    update

---
+++ resources.tf
@@ -0,0 +1,16 @@
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+module "nginx" {
+  source = "./modules/nginx"
+  depends_on = [null_resource.k3s_status] 
+}
+
+module "neuvector" {
+  source = "./modules/neuvector"
+  depends_on = [module.nginx] 
+}
